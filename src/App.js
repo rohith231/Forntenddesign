e4319e9e15c8c35ff900f1d389bf384bba768f4a
rohith231 <68227450+rohith231@users.noreply.github.com>
2020-07-30 19:23:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+
+import { useSelector } from 'react-redux';
+import React, { Fragment, useEffect } from 'react';
+import Navbar from './components/layout/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+
+
+import './App.css';
+
+
+
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
+
+  const flight = useSelector((state) => state.flights);
+ console.log("App: "+JSON.stringify(flight));
+ 
+  return (
+    <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          
+         <Route exact path="/" component={Landing} />
+        <PrivateRoute exact path="/DynamicForm"  component={Sidebar}/>
+        <Route component={Routes} />
+        </Switch>
+      </Fragment>
+    </Router>
+  </Provider>
+  
+  );
+}
+
+
+
+
+
+export default App;
+
+
+
